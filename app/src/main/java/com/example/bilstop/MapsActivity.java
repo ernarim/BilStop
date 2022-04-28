0b2ca0c70a677ff89f0137e9bc138c428e4eac2d
Eren <erenarim7b@gmail.com>
2022-04-29 01:11:41 +0300

    Added ability to select a route inside multiple routes

--- app/src/main/java/com/example/bilstop/MapsActivity.java
+++ app/src/main/java/com/example/bilstop/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.bilstop;
 
+import com.example.bilstop.Models.PolylineData;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.MapsInitializer.Renderer;
 import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -29,13 +32,16 @@ import com.google.maps.internal.PolylineEncoding;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.DirectionsRoute;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
 
     private GeoApiContext mGeoApiContext =null;
     private GoogleMap googleMap;
+    private ArrayList<PolylineData> mPolyLinesData = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,32 +66,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // and move the map's camera to the same location.
 
         googleMap=gMap;
+
         LatLng ankara = new LatLng(39.87143940863815, 32.754316941660086);
         MarkerOptions marker = new MarkerOptions().position(ankara).title("Bilkent");
         googleMap.addMarker(marker);
 
-        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
-                marker.getPosition().latitude,
-                marker.getPosition().longitude
-        );
-
-        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
-        directions.alternatives(true);
-        directions.origin(
-                new com.google.maps.model.LatLng(39.91034784682875, 32.78769906699375)
-        );
-        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
-            @Override
-            public void onResult(DirectionsResult result) {
-                Log.d("demo", "routes " + result.routes[0].toString());
-                addPolylinesToMap(result);
-            }
-
-            @Override
-            public void onFailure(Throwable e) {
-
-            }
-        });
+        calculateDirections(marker);
 
         GoogleMapOptions options = new GoogleMapOptions();
 
@@ -96,6 +82,72 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         googleMap.getUiSettings().setZoomControlsEnabled(true);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(ankara));
+
+        googleMap.setOnPolylineClickListener(this);
+
+        // add marker on the screen when touch somewhere on the map
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                googleMap.clear();
+
+                // Animating to the touched position
+                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                googleMap.addMarker(markerOptions);
+            }
+        });
+
+
+    }
+
+    private void calculateDirections(MarkerOptions marker){
+        Log.d("demo", "calculateDirections: calculating directions.");
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                marker.getPosition().latitude,
+                marker.getPosition().longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        directions.alternatives(true);
+        directions.origin(
+                new com.google.maps.model.LatLng(39.92072971801894, 32.854265323610015)
+        );
+
+        Log.d("demo", "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+//                Log.d(TAG, "calculateDirections: routes: " + result.routes[0].toString());
+//                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].duration);
+//                Log.d(TAG, "calculateDirections: distance: " + result.routes[0].legs[0].distance);
+//                Log.d(TAG, "calculateDirections: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+
+                Log.d("demo", "onResult: successfully retrieved directions.");
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e("demo", "calculateDirections: Failed to get directions: " + e.getMessage() );
+
+            }
+        });
     }
 
     private void addPolylinesToMap(final DirectionsResult result){
@@ -103,6 +155,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void run() {
                 Log.d("demo", "run: result routes: " + result.routes.length);
+                if(mPolyLinesData.size()>0){
+                    for(PolylineData polylineData: mPolyLinesData){
+                        polylineData.getPolyline().remove();
+                    }
+                    mPolyLinesData.clear();
+                    mPolyLinesData = new ArrayList<>();
+                }
 
                 for(DirectionsRoute route: result.routes){
                     Log.d("demo", "run: leg: " + route.legs[0].toString());
@@ -122,12 +181,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
 
                     Polyline polyline = googleMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
-                    polyline.setColor(ContextCompat.getColor(getApplicationContext(), R.color.orange));
+                    polyline.setColor(ContextCompat.getColor(getApplicationContext(), R.color.grew));
                     polyline.setClickable(true);
+                    mPolyLinesData.add(new PolylineData(polyline,route.legs[0]));
 
                 }
             }
         });
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+
+        for(PolylineData polylineData: mPolyLinesData){
+            Log.d("demo", "onPolylineClick: toString: " + polylineData.toString());
+            if(polyline.getId().equals(polylineData.getPolyline().getId())){
+                polylineData.getPolyline().setColor(ContextCompat.getColor(getApplicationContext(), R.color.orange));
+                polylineData.getPolyline().setZIndex(1);
+            }
+            else{
+                polylineData.getPolyline().setColor(ContextCompat.getColor(getApplicationContext(), R.color.grew));
+                polylineData.getPolyline().setZIndex(0);
+            }
+        }
     }
 
 
